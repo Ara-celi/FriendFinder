9e30970b9554db030c177e96a8d507ff536e5ad0
Araceli Badillo <badilloaraceli@gmail.com>
2017-01-22 19:17:07 -0500

    init push

---
+++ app/data/friends.js
@@ -0,0 +1,96 @@
+var friends = [
+{
+  "name":"Samuel",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+     5,
+     1,
+     4,
+     4,
+     5,
+     1,
+     2,
+     5,
+     4,
+     1
+   ]
+},{
+  "name":"Michael",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+     5,
+     1,
+     3,
+     4,
+     5,
+     1,
+     3,
+     5,
+     4,
+     5
+   ]
+},{
+  "name":"Victor",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+     5,
+     1,
+     1,
+     4,
+     5,
+     1,
+     1,
+     5,
+     3,
+     1
+   ]
+},{
+  "name":"Lorry",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+     5,
+     1,
+     5,
+     4,
+     5,
+     1,
+     3,
+     5,
+     4,
+     4
+   ]
+},{
+  "name":"Crystal",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+     1,
+     5,
+     4,
+     2,
+     5,
+     1,
+     2,
+     1,
+     4,
+     1
+   ]
+},{
+  "name":"Andre",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+     3,
+     1,
+     4,
+     4,
+     3,
+     1,
+     2,
+     3,
+     4,
+     1
+   ]
+}
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
